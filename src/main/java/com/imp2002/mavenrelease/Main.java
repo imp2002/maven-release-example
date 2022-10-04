e7d72e8908503ae7a624cbbe4c3b7f4066b6f8c8
imp2002 <imp07@qq.com>
2022-10-04 17:50:35 +0800

    feat: test

---
+++ src/main/java/com/imp2002/mavenrelease/Main.java
@@ -0,0 +1,11 @@
+
+package com.imp2002.mavenrelease;
+
+/**
+ * @author imp2002
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("maven release test!");
+    }
+}
---
+++ src/test/java/com/imp2002/mavenrelease/ReleaseTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+/**
+ * @author imp2002
+ * @date 2022-10-04 17:33
+ */
+
+public class ReleaseTest {
+    @Test
+    public void releaseTest() {
+        System.out.println("maven release test!");
+    }
+}
